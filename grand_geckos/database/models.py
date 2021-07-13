325b6f3e2e796b743ed6682dc8a705393dc414bb
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2021-07-13 22:59:02 +0200

    Add Database features and Cryptography to Poetry
    Implement Fernet-symmetric encryption in `User` model, and add
    Cryptography to the Poetry enviroment

--- grand_geckos/database/models.py
+++ grand_geckos/database/models.py
@@ -1,3 +1,10 @@
+from base64 import urlsafe_b64encode
+from datetime import datetime
+from os import urandom
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import relationship
@@ -8,32 +15,32 @@ Base: DeclarativeMeta = declarative_base()
 class User(Base):
     """Class representing a local user of the 'Secret Crate of Grand Geckos'."""
 
-    __tablename__ = "users"
+    __tablename__ = "User"
     id = Column(Integer, primary_key=True)
     username = Column(String)
     password = Column(String)
     salt = Column(String)
-    credentials_id = relationship("credentials")
+    credentials = relationship("Credential")
     last_login = Column(DateTime)
+
+    def __init__(self, username: str, password: str) -> None:
+        salt_decoded = urandom(16)
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt_decoded, iterations=10000)
+        key = urlsafe_b64encode(kdf.derive((password.encode("utf-8"))))
+        f = Fernet(key)
+        self.username = username
+        self.password = f.encrypt(password.encode("utf-8")).decode("utf-8")
+        self.salt = urlsafe_b64encode(salt_decoded).decode("utf-8")
+        self.last_login = datetime.now()
 
 
 class Credential(Base):
     """Class representing a credential of a local user of the 'Secret Crate of Grand Geckos'."""
 
-    __tablename__ = "credentials"
+    __tablename__ = "Credential"
     id = Column(Integer, primary_key=True)
     credential_name = Column(String)
     credential_username = Column(String)
     credential_password = Column(String)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    platform_id = Column(Integer, ForeignKey("platforms.id"))
-    platform = relationship("Platform", back_populates="credential")
-
-
-class Platform(Base):
-    """Class representing a Platform that can be added to the 'Secret Crate of Grand Geckos'."""
-
-    __tablename__ = "platforms"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    credential = relationship("Credential", back_populates="platforms")
+    user_id = Column(Integer, ForeignKey("User.id"))
+    platform = Column(String)
