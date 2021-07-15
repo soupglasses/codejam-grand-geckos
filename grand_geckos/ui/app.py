0c7d7894a18b8beba0ec66acecdaa47041805a25
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-07-15 17:42:02 +0530

    add auth_handler and redo run_app function

--- grand_geckos/ui/app.py
+++ grand_geckos/ui/app.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from prompt_toolkit.application import Application
 from prompt_toolkit.shortcuts.dialogs import message_dialog
 
@@ -7,25 +9,28 @@ from grand_geckos.ui.dashboard import get_app as dashboard_app
 app: Application = None
 
 
-def run_app(case=None):
+def run_app(case=None, title_text=None, message_text=None):
+    """handle running of multiple pages/apps"""
     global app
 
-    if case == -1:
+    if case == 0:
+        # display login page with an optional message dialog before
+        if title_text is not None and message_text is not None:
+            message_dialog(title=title_text, text=message_text).run()
         app = login_dialog()
 
     if case == 1:
+        # display dashboard page with an optional message dialog before
+        if title_text is not None and message_text is not None:
+            message_dialog(title=title_text, text=message_text).run()
         app = dashboard_app()
 
-    if case == 0:
-        message_dialog(title="SUCCESS", text="successfully registered").run()
-        app = login_dialog()
-
-    if case == 2:
-        message_dialog(title="TRY AGAIN", text="incorrect username/password").run()
-        app = login_dialog()
-
-    if case is None:
+    response: Dict[str, Any] = app.run()
+    if response is None:
+        # exit app
         return
 
-    resp = app.run()
-    run_app(resp)
+    res_case = response.get("case")
+    res_title_text = response.get("title")
+    res_message_text = response.get("text")
+    run_app(res_case, res_title_text, res_message_text)
