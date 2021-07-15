07d2b57cd545bcad1a7bc6bba390d2c2378e6247
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2021-07-15 17:34:37 +0200

    Integrate the DB and Operations into the UI
    Integrate the DB and Operations into the UI
    - Rework the Login/Register Panel
    - Add dynamic data from the DB to the layout

--- grand_geckos/ui/auth.py
+++ grand_geckos/ui/auth.py
@@ -1,90 +1,55 @@
-from typing import Any, Optional
+from prompt_toolkit.shortcuts import button_dialog, input_dialog, message_dialog
 
-from prompt_toolkit.application import Application
-from prompt_toolkit.application.current import get_app
-from prompt_toolkit.buffer import Buffer
-from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
-from prompt_toolkit.key_binding.defaults import load_key_bindings
-from prompt_toolkit.key_binding.key_bindings import KeyBindings, merge_key_bindings
-from prompt_toolkit.layout import Layout
-from prompt_toolkit.layout.containers import AnyContainer, HSplit
-from prompt_toolkit.layout.dimension import Dimension as D
-from prompt_toolkit.styles import BaseStyle
-from prompt_toolkit.widgets import Button, Dialog, Label, TextArea, ValidationToolbar
-
-from grand_geckos.utils.auth_handler import handle_login_event, handle_register_event
+from grand_geckos.database.DBWorker import DatabaseWorker
+from grand_geckos.database.exceptions import AuthenticationError, UserAlreadyExistsError
+from grand_geckos.ui.dashboard import get_app as dashboard_app
 
 
-def login_dialog(style: Optional[BaseStyle] = None) -> Application[str]:
-    """
-    Returns the login / register dialog application
-    """
+def run_app_init():
+    while True:
+        result = button_dialog(
+            title="Welcome",
+            text="Please choose an option.",
+            buttons=[("Register", True), ("Login", False), ("Exit", None)],
+        ).run()
+        if result is True:
+            text_username = input_dialog(title="Register - Username", text="Please type your username").run()
+            if text_username is None:
+                continue
+            text_pass = input_dialog(
+                title="Register - Password", text="Please type your password:", password=True
+            ).run()
+            if text_pass is None:
+                continue
+            text_pass_confirm = input_dialog(
+                title="Register - Password-Confirm", text="Please type your password again:", password=True
+            ).run()
+            if text_pass_confirm is None:
+                continue
+            try:
+                worker = DatabaseWorker.create_user(
+                    username=text_username, password=text_pass, password_confirm=text_pass_confirm
+                )
+            except UserAlreadyExistsError as e:
+                message_dialog(title="Error!", text=str(e)).run()
+                continue
+            message_dialog(title="Successful registration!", text="").run()
+            dashboard_app(worker).run()
 
-    def accept(buf: Buffer) -> bool:
-        get_app().layout.focus(login_button)
-        return True
+        elif result is False:
+            text_username = input_dialog(title="Login - Username", text="Please type your username").run()
+            if text_username is None:
+                continue
+            text_pass = input_dialog(title="Login - Password", text="Please type your password:", password=True).run()
+            if text_pass is None:
+                continue
+            try:
+                worker = DatabaseWorker.auth_user(username=text_username, password=text_pass)
+            except AuthenticationError as e:
+                message_dialog(title="Error!", text=str(e)).run()
+                continue
 
-    def register_handler() -> None:
-        # TODO: add password confirmation
-        case, title, text = handle_register_event(username.text, password.text)
-        get_app().exit(result={"case": case, "title": title, "text": text})
-
-    def login_handler() -> None:
-        case, title, text = handle_login_event(username.text, password.text)
-        get_app().exit(result={"case": case, "title": title, "text": text})
-
-    def exit_handler() -> None:
-        get_app().exit()
-
-    register_button = Button(text="Register", handler=register_handler)
-    login_button = Button(text="Login", handler=login_handler)
-    exit_button = Button(text="Exit", handler=exit_handler)
-
-    username = TextArea(
-        multiline=False,
-        password=False,
-        completer=None,
-        validator=None,
-        accept_handler=accept,
-    )
-
-    password = TextArea(
-        multiline=False,
-        password=True,
-        completer=None,
-        validator=None,
-        accept_handler=accept,
-    )
-
-    dialog = Dialog(
-        title="Login",
-        body=HSplit(
-            [
-                Label(text="Username", dont_extend_height=True),
-                username,
-                Label(text="Password", dont_extend_height=True),
-                password,
-                ValidationToolbar(),
-            ],
-            padding=D(preferred=1, max=1),
-        ),
-        buttons=[register_button, login_button, exit_button],
-        with_background=True,
-    )
-
-    return _create_app(dialog, style)
-
-
-def _create_app(dialog: AnyContainer, style: Optional[BaseStyle]) -> Application[Any]:
-    # Key bindings.
-    bindings = KeyBindings()
-    bindings.add("tab")(focus_next)
-    bindings.add("s-tab")(focus_previous)
-
-    return Application(
-        layout=Layout(dialog),
-        key_bindings=merge_key_bindings([load_key_bindings(), bindings]),
-        mouse_support=True,
-        style=style,
-        full_screen=True,
-    )
+            message_dialog(title="Successful login!", text="").run()
+            dashboard_app(worker).run()
+        elif result is None:
+            exit()
